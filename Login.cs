9b5da794cca75e3116900ea3c95ce92176d84bd9
nguyendacanh1997dn <nguyendacanh1997dn1@gmail.com>
2017-02-16 14:39:01 +0700

    source code

---
+++ Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CfManage
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thực sự muốn thoát ?", "Thông báo", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            MainForm f = new MainForm();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+
+        }
+    }
+}
